63486a0870e1f92e25a5024622006dfc107b578a
agmo <mor.agnieszka@gmail.com>
2019-12-09 22:40:50 +0100

    AofC 2019, Day8, part 2

--- 2019/day8.js
+++ 2019/day8.js
@@ -20,3 +20,27 @@ function day8_part1(digits) {
 
   return (ones && ones.length || 1) * (twos && twos.length || 1);
 }
+
+function day8_part2(digits) {
+  const pxWidth = 25;
+  const pxHeight = 6;
+  let layer = '';
+
+  for (let i = 0; i < pxWidth * pxHeight; i++) {
+    for (let j = 0; j < digits.length; j += pxWidth * pxHeight) {
+      if (digits[i + j] === '2') {
+        continue;
+      }
+
+      layer += digits[i + j];
+
+      if (layer.length === pxWidth) {
+        console.log(layer);
+        layer = ''
+      }
+      break;
+    }
+  }
+
+  return true; // Return true for QUnit only; puzzle answer gets logged to the console.
+}
