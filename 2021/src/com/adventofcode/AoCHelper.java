285741f308a7f754e776e4f8ab831d0f9ceafed8
agmo <mor.agnieszka@gmail.com>
2021-11-29 21:48:05 +0100

    Init 2021

---
+++ 2021/src/com/adventofcode/AoCHelper.java
@@ -0,0 +1,29 @@
+package com.adventofcode;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AoCHelper {
+    public static List<String> getPuzzleInput(int day) {
+        var fullPath = Paths.get("puzzleInputs", String.format("day%s.txt", day));
+        var result = new ArrayList<String>();
+
+        try {
+            Scanner scanner = new Scanner(new File(fullPath.toString()));
+            while (scanner.hasNextLine()) {
+                result.add(scanner.nextLine());
+            }
+            scanner.close();
+
+            return result;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+}
---
+++ 2021/src/com/adventofcode/Main.java
@@ -0,0 +1,8 @@
+package com.adventofcode;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(AoCHelper.getPuzzleInput(1));
+    }
+}
