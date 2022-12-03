75b672ea06ad83d6289f27c83a6e64619fd5f244
agmo <mor.agnieszka@gmail.com>
2022-12-03 10:41:20 +0100

    Day 3, part 2

--- 2022/day3.js
+++ 2022/day3.js
@@ -19,15 +19,35 @@ function calculatePrioritiesSumPart1(rucksackContents) {
 
 
   return totalPriority;
+}
 
-  function calculatePriority(type) {
-    const charCode = type.charCodeAt(0);
-    const isSmallLetter = charCode >= 97 && charCode <= 122;
+function calculatePrioritiesSumPart2(rucksackContents) {
+  let totalPriority = 0;
 
-    if (isSmallLetter) {
-      return charCode - 96;
-    } else {
-      return charCode - 38;
+  for (let i = 0; i < rucksackContents.length; i = i + 3) {
+    let commonType;
+
+    for (const type of rucksackContents[i]) {
+      if (rucksackContents[i + 1].includes(type) && rucksackContents[i + 2].includes(type)) {
+        commonType = type;
+        break
+      }
     }
+
+    const commonTypePriority = calculatePriority(commonType)
+    totalPriority += commonTypePriority;
+  }
+
+  return totalPriority;
+}
+
+function calculatePriority(type) {
+  const charCode = type.charCodeAt(0);
+  const isSmallLetter = charCode >= 97 && charCode <= 122;
+
+  if (isSmallLetter) {
+    return charCode - 96;
+  } else {
+    return charCode - 38;
   }
 }
