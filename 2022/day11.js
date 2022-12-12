52195da05fff67d80dd4eaa19b1e386b42b3fe07
agmo <mor.agnieszka@gmail.com>
2022-12-12 18:27:27 +0100

    Day 11, part 2

--- 2022/day11.js
+++ 2022/day11.js
@@ -1,14 +1,15 @@
-function findLevelOfMonkeyBusiness(notes) {
-  const rounds = 20;
+function findLevelOfMonkeyBusiness(notes, part) {
+  const rounds = part === 1 ? 20 : 10000;
+  const allDivisibleBy = notes.map(note => note.test).reduce((a, b) => a * b, 1);
 
   for (let i = 0; i < rounds; i++) {
     notes.forEach(note => {
       note.startingItems.forEach(item => {
         note.inspectionCount++;
         const postInspectionWorryLevel = note.operation(item);
-        const worryLevelWithRelief = applyRelief(postInspectionWorryLevel);
-        const monkeyToThrowTo = note.monkeyToThrowTo(worryLevelWithRelief);
-        notes[monkeyToThrowTo].startingItems.push(worryLevelWithRelief);
+        const worryLevelToThrow = part === 1 ? applyRelief(postInspectionWorryLevel) : keepWorryLevelsManageable(postInspectionWorryLevel);
+        const monkeyToThrowTo = note.monkeyToThrowTo(worryLevelToThrow);
+        notes[monkeyToThrowTo].startingItems.push(worryLevelToThrow);
       });
       note.startingItems = [];
     });
@@ -20,5 +21,10 @@ function findLevelOfMonkeyBusiness(notes) {
 
   function applyRelief(worryLevel) {
     return Math.floor(worryLevel / 3);
+  }
+
+  function keepWorryLevelsManageable(worryLevel) {
+    //https://en.wikipedia.org/wiki/Modular_arithmetic
+    return worryLevel % allDivisibleBy;
   }
 }
--- 2022/day11_input.js
+++ 2022/day11_input.js
@@ -2,6 +2,7 @@ const testNotes = [
   {
     startingItems: [79, 98],
     operation: worryLevel => worryLevel * 19,
+    test: 23,
     monkeyToThrowTo: worryLevel => {
       if (worryLevel % 23 === 0) {
         return 2;
@@ -14,6 +15,7 @@ const testNotes = [
   {
     startingItems: [54, 65, 75, 74],
     operation: worryLevel => worryLevel + 6,
+    test: 19,
     monkeyToThrowTo: worryLevel => {
       if (worryLevel % 19 === 0) {
         return 2;
@@ -26,6 +28,7 @@ const testNotes = [
   {
     startingItems: [79, 60, 97],
     operation: worryLevel => worryLevel * worryLevel,
+    test: 13,
     monkeyToThrowTo: worryLevel => {
       if (worryLevel % 13 === 0) {
         return 1;
@@ -38,6 +41,7 @@ const testNotes = [
   {
     startingItems: [74],
     operation: worryLevel => worryLevel + 3,
+    test: 17,
     monkeyToThrowTo: worryLevel => {
       if (worryLevel % 17 === 0) {
         return 0;
@@ -53,6 +57,7 @@ const notes = [
   {
     startingItems: [54, 61, 97, 63, 74],
     operation: worryLevel => worryLevel * 7,
+    test: 17,
     monkeyToThrowTo: worryLevel => {
       if (worryLevel % 17 === 0) {
         return 5;
@@ -65,6 +70,7 @@ const notes = [
   {
     startingItems: [61, 70, 97, 64, 99, 83, 52, 87],
     operation: worryLevel => worryLevel + 8,
+    test: 2,
     monkeyToThrowTo: worryLevel => {
       if (worryLevel % 2 === 0) {
         return 7;
@@ -77,6 +83,7 @@ const notes = [
   {
     startingItems: [60, 67, 80, 65],
     operation: worryLevel => worryLevel * 13,
+    test: 5,
     monkeyToThrowTo: worryLevel => {
       if (worryLevel % 5 === 0) {
         return 1;
@@ -89,6 +96,7 @@ const notes = [
   {
     startingItems: [61, 70, 76, 69, 82, 56],
     operation: worryLevel => worryLevel + 7,
+    test: 3,
     monkeyToThrowTo: worryLevel => {
       if (worryLevel % 3 === 0) {
         return 5;
@@ -101,6 +109,7 @@ const notes = [
   {
     startingItems: [79, 98],
     operation: worryLevel => worryLevel + 2,
+    test: 7,
     monkeyToThrowTo: worryLevel => {
       if (worryLevel % 7 === 0) {
         return 0;
@@ -113,6 +122,7 @@ const notes = [
   {
     startingItems: [72, 79, 55],
     operation: worryLevel => worryLevel + 1,
+    test: 13,
     monkeyToThrowTo: worryLevel => {
       if (worryLevel % 13 === 0) {
         return 2;
@@ -125,6 +135,7 @@ const notes = [
   {
     startingItems: [63],
     operation: worryLevel => worryLevel + 4,
+    test: 19,
     monkeyToThrowTo: worryLevel => {
       if (worryLevel % 19 === 0) {
         return 7;
@@ -137,6 +148,7 @@ const notes = [
   {
     startingItems: [72, 51, 93, 63, 80, 86, 81],
     operation: worryLevel => worryLevel * worryLevel,
+    test: 11,
     monkeyToThrowTo: worryLevel => {
       if (worryLevel % 11 === 0) {
         return 0;
